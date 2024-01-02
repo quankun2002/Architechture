7a85c98981dc9fcab37e8c395518f89f95c82bd3
quankun <s3877457@rmit.edu.vn>
2024-01-02 19:14:56 +0700

    fix some bug on upload

--- static/js/upload.js
+++ static/js/upload.js
@@ -1,22 +1,21 @@
-const fileInputElement = document.querySelector("input#input1");
+const fileInputElement = document.querySelector("input1");
 const clearButton = document.querySelector(".clear_button");
 const imagePreview = document.querySelector(".preview-image");
 const previewDescription = document.querySelector(".preview-description");
 const previewSize = document.querySelector(".preview-size");
-
 const changePreviewOnUpload = (fileName, fileType, fileSize, previewURL) => {
   if (fileType.includes("image")) {
     imagePreview.setAttribute("src", previewURL);
   } else {
-    imagePreview.setAttribute("src", "images/placeholder-file.png");
+    imagePreview.setAttribute("src", imageUrl2);
   }
   previewDescription.textContent = fileName;
   previewSize.textContent = `${Math.round(fileSize / 1024)} kb`;
 
   clearButton.style.display = "inline-flex";
 };
-
-fileInputElement.addEventListener("change", (e) => {
+document.getElementById('input1').addEventListener('change', function() {
+  const fileInputElement = document.getElementById('input1');
   console.log(fileInputElement.files[0]);
   const fileObject = fileInputElement.files[0];
   const objectURL = URL.createObjectURL(fileObject);
@@ -26,12 +25,25 @@ fileInputElement.addEventListener("change", (e) => {
     fileObject.name,
     fileObject.type,
     fileObject.size,
-    objectURL
-  );
+    objectURL);
+  document.getElementById('uploadForm').submit();
 });
+// fileInputElement.addEventListener("change", (e) => {
+//   console.log(fileInputElement.files[0]);
+//   const fileObject = fileInputElement.files[0];
+//   const objectURL = URL.createObjectURL(fileObject);
+//   console.log(objectURL);
+
+//   changePreviewOnUpload(
+//     fileObject.name,
+//     fileObject.type,
+//     fileObject.size,
+//     objectURL
+//   );
+// });
 
 clearButton.addEventListener("click", (e) => {
-  imagePreview.setAttribute("src", "images/placeholder-image.png");
+  imagePreview.setAttribute("src", imageUrl);
 
   previewDescription.textContent = "";
   previewSize.textContent = "";
