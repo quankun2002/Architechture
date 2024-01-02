637584b3c074fcde36b937dbd765d9757e1072c3
quankun <s3877457@rmit.edu.vn>
2024-01-02 16:57:27 +0700

    more rearrangement and flask for login

--- main.py
+++ main.py
@@ -4,11 +4,14 @@ from flask import Flask, render_template, request, redirect, url_for, send_file
 app = Flask(__name__)
 
 @app.route('/')
-def list_files():
+def main():
    return render_template('index.html')
 @app.route('/login')
 def login():
     return render_template('login.html')
+@app.route('/userDetail')
+def userDetail():
+    return render_template('userDetail.html')
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
