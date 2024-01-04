9535a53bc33afa0d59831d560d89dc5314c45173
thuyiswater <s3877746@rmit.edu.vn>
2024-01-04 11:09:39 +0700

    run web

--- main.py
+++ main.py
@@ -2,17 +2,22 @@
 import os
 from flask import Flask, render_template, request, redirect, url_for, send_file, make_response
 from werkzeug.utils import secure_filename
-app = Flask(__name__)
+
+app = Flask(__name__, template_folder='templates')
 app.config['UPLOAD_FOLDER'] = r'temp'  # replace with your upload directory
+
 @app.route('/')
 def main():
    return render_template('index.html')
+
 @app.route('/login')
 def login():
     return render_template('login.html')
+
 @app.route('/userDetail')
 def userDetail():
     return render_template('userDetail.html')
+
 @app.route('/upload', methods=['GET', 'POST'])
 def upload():
     if request.method == 'POST':
@@ -28,5 +33,6 @@ def upload():
             return response
             #os.remove(os.path.join(app.config['UPLOAD_FOLDER'], filename))
     else: return render_template('upload.html')
+
 if __name__ == '__main__':
     app.run(host='127.0.0.1')
